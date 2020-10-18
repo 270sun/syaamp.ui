3bbe14ae3a7a56cc85288f33571ee33ea11529a8
Ming Sun （孙铭） <sunm@tesla.com>
2020-10-18 19:01:16 +0800

    init

---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+import { Config } from '@abp/ng.core';
+
+const baseUrl = 'http://localhost:4200';
+
+export const environment = {
+  production: false,
+  application: {
+    baseUrl,
+    name: 'AssetManagementPlatform',
+    logoUrl: '',
+  },
+  oAuthConfig: {
+    issuer: 'https://localhost:44346',
+    redirectUri: baseUrl,
+    clientId: 'AssetManagementPlatform_App',
+    responseType: 'code',
+    scope: 'offline_access AssetManagementPlatform',
+  },
+  apis: {
+    default: {
+      url: 'https://localhost:44373',
+      rootNamespace: 'SY.Airport.AssetManagementPlatform',
+    },
+  },
+} as Config.Environment;
